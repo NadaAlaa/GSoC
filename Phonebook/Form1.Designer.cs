a076df88689784258769e8c68541221b81535e7a
Nada <nadaalaa.na@gmail.com>
2015-05-01 02:01:20 +0200

    Update Form1.Designer.cs

--- Phonebook/Form1.Designer.cs
+++ Phonebook/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Phonebook
+namespace Phonebook
 {
     partial class Form1
     {
@@ -28,12 +28,250 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AddNewPerson_Label = new System.Windows.Forms.Label();
+            this.DeleteContact_Label = new System.Windows.Forms.Label();
+            this.AddNewContact_Label = new System.Windows.Forms.Label();
+            this.AddNewPersonAddress_Label = new System.Windows.Forms.Label();
+            this.AddNewPersonName_Label = new System.Windows.Forms.Label();
+            this.AddPersonID_Label = new System.Windows.Forms.Label();
+            this.AddNewContactID_Label = new System.Windows.Forms.Label();
+            this.DeleteContactID_Label = new System.Windows.Forms.Label();
+            this.AddNewPersonID_TextBox = new System.Windows.Forms.TextBox();
+            this.DeleteContactID_TextBox = new System.Windows.Forms.TextBox();
+            this.AddNewContactID_TextBox = new System.Windows.Forms.TextBox();
+            this.AddNewPersonAddress_TextBox = new System.Windows.Forms.TextBox();
+            this.AddNewPersonName_TextBox = new System.Windows.Forms.TextBox();
+            this.AddNewPerson_Button = new System.Windows.Forms.Button();
+            this.DeletePerson_Button = new System.Windows.Forms.Button();
+            this.AddNewContact_Button = new System.Windows.Forms.Button();
+            this.AddNewContact_TextBox = new System.Windows.Forms.TextBox();
+            this.PhoneType_ComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // AddNewPerson_Label
+            // 
+            this.AddNewPerson_Label.AutoSize = true;
+            this.AddNewPerson_Label.Font = new System.Drawing.Font("Quartz MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNewPerson_Label.Location = new System.Drawing.Point(11, 9);
+            this.AddNewPerson_Label.Name = "AddNewPerson_Label";
+            this.AddNewPerson_Label.Size = new System.Drawing.Size(155, 19);
+            this.AddNewPerson_Label.TabIndex = 0;
+            this.AddNewPerson_Label.Text = "Add New Person";
+            this.AddNewPerson_Label.UseMnemonic = false;
+            // 
+            // DeleteContact_Label
+            // 
+            this.DeleteContact_Label.AutoSize = true;
+            this.DeleteContact_Label.Font = new System.Drawing.Font("Quartz MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteContact_Label.Location = new System.Drawing.Point(14, 238);
+            this.DeleteContact_Label.Name = "DeleteContact_Label";
+            this.DeleteContact_Label.Size = new System.Drawing.Size(154, 19);
+            this.DeleteContact_Label.TabIndex = 1;
+            this.DeleteContact_Label.Text = "Delete Contact";
+            this.DeleteContact_Label.UseMnemonic = false;
+            // 
+            // AddNewContact_Label
+            // 
+            this.AddNewContact_Label.AutoSize = true;
+            this.AddNewContact_Label.Font = new System.Drawing.Font("Quartz MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNewContact_Label.Location = new System.Drawing.Point(12, 138);
+            this.AddNewContact_Label.Name = "AddNewContact_Label";
+            this.AddNewContact_Label.Size = new System.Drawing.Size(167, 19);
+            this.AddNewContact_Label.TabIndex = 2;
+            this.AddNewContact_Label.Text = "Add New Contact";
+            this.AddNewContact_Label.UseMnemonic = false;
+            // 
+            // AddNewPersonAddress_Label
+            // 
+            this.AddNewPersonAddress_Label.AutoSize = true;
+            this.AddNewPersonAddress_Label.Font = new System.Drawing.Font("Quartz MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNewPersonAddress_Label.Location = new System.Drawing.Point(14, 97);
+            this.AddNewPersonAddress_Label.Name = "AddNewPersonAddress_Label";
+            this.AddNewPersonAddress_Label.Size = new System.Drawing.Size(85, 19);
+            this.AddNewPersonAddress_Label.TabIndex = 3;
+            this.AddNewPersonAddress_Label.Text = "Address";
+            this.AddNewPersonAddress_Label.UseMnemonic = false;
+            // 
+            // AddNewPersonName_Label
+            // 
+            this.AddNewPersonName_Label.AutoSize = true;
+            this.AddNewPersonName_Label.Font = new System.Drawing.Font("Quartz MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNewPersonName_Label.Location = new System.Drawing.Point(14, 70);
+            this.AddNewPersonName_Label.Name = "AddNewPersonName_Label";
+            this.AddNewPersonName_Label.Size = new System.Drawing.Size(58, 19);
+            this.AddNewPersonName_Label.TabIndex = 4;
+            this.AddNewPersonName_Label.Text = "Name";
+            this.AddNewPersonName_Label.UseMnemonic = false;
+            // 
+            // AddPersonID_Label
+            // 
+            this.AddPersonID_Label.AutoSize = true;
+            this.AddPersonID_Label.Font = new System.Drawing.Font("Quartz MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddPersonID_Label.Location = new System.Drawing.Point(14, 43);
+            this.AddPersonID_Label.Name = "AddPersonID_Label";
+            this.AddPersonID_Label.Size = new System.Drawing.Size(25, 19);
+            this.AddPersonID_Label.TabIndex = 5;
+            this.AddPersonID_Label.Text = "ID";
+            this.AddPersonID_Label.UseMnemonic = false;
+            // 
+            // AddNewContactID_Label
+            // 
+            this.AddNewContactID_Label.AutoSize = true;
+            this.AddNewContactID_Label.Font = new System.Drawing.Font("Quartz MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNewContactID_Label.Location = new System.Drawing.Point(14, 170);
+            this.AddNewContactID_Label.Name = "AddNewContactID_Label";
+            this.AddNewContactID_Label.Size = new System.Drawing.Size(25, 19);
+            this.AddNewContactID_Label.TabIndex = 6;
+            this.AddNewContactID_Label.Text = "ID";
+            this.AddNewContactID_Label.UseMnemonic = false;
+            // 
+            // DeleteContactID_Label
+            // 
+            this.DeleteContactID_Label.AutoSize = true;
+            this.DeleteContactID_Label.Font = new System.Drawing.Font("Quartz MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteContactID_Label.Location = new System.Drawing.Point(14, 279);
+            this.DeleteContactID_Label.Name = "DeleteContactID_Label";
+            this.DeleteContactID_Label.Size = new System.Drawing.Size(25, 19);
+            this.DeleteContactID_Label.TabIndex = 7;
+            this.DeleteContactID_Label.Text = "ID";
+            this.DeleteContactID_Label.UseMnemonic = false;
+            // 
+            // AddNewPersonID_TextBox
+            // 
+            this.AddNewPersonID_TextBox.Location = new System.Drawing.Point(162, 43);
+            this.AddNewPersonID_TextBox.Name = "AddNewPersonID_TextBox";
+            this.AddNewPersonID_TextBox.Size = new System.Drawing.Size(320, 21);
+            this.AddNewPersonID_TextBox.TabIndex = 8;
+            // 
+            // DeleteContactID_TextBox
+            // 
+            this.DeleteContactID_TextBox.Location = new System.Drawing.Point(162, 279);
+            this.DeleteContactID_TextBox.Name = "DeleteContactID_TextBox";
+            this.DeleteContactID_TextBox.Size = new System.Drawing.Size(320, 21);
+            this.DeleteContactID_TextBox.TabIndex = 9;
+            // 
+            // AddNewContactID_TextBox
+            // 
+            this.AddNewContactID_TextBox.Location = new System.Drawing.Point(162, 170);
+            this.AddNewContactID_TextBox.Name = "AddNewContactID_TextBox";
+            this.AddNewContactID_TextBox.Size = new System.Drawing.Size(320, 21);
+            this.AddNewContactID_TextBox.TabIndex = 10;
+            // 
+            // AddNewPersonAddress_TextBox
+            // 
+            this.AddNewPersonAddress_TextBox.Location = new System.Drawing.Point(162, 97);
+            this.AddNewPersonAddress_TextBox.Name = "AddNewPersonAddress_TextBox";
+            this.AddNewPersonAddress_TextBox.Size = new System.Drawing.Size(320, 21);
+            this.AddNewPersonAddress_TextBox.TabIndex = 11;
+            // 
+            // AddNewPersonName_TextBox
+            // 
+            this.AddNewPersonName_TextBox.Location = new System.Drawing.Point(162, 70);
+            this.AddNewPersonName_TextBox.Name = "AddNewPersonName_TextBox";
+            this.AddNewPersonName_TextBox.Size = new System.Drawing.Size(320, 21);
+            this.AddNewPersonName_TextBox.TabIndex = 12;
+            this.AddNewPersonName_TextBox.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // AddNewPerson_Button
+            // 
+            this.AddNewPerson_Button.Location = new System.Drawing.Point(510, 97);
+            this.AddNewPerson_Button.Name = "AddNewPerson_Button";
+            this.AddNewPerson_Button.Size = new System.Drawing.Size(75, 23);
+            this.AddNewPerson_Button.TabIndex = 13;
+            this.AddNewPerson_Button.Text = "Add";
+            this.AddNewPerson_Button.UseVisualStyleBackColor = true;
+            this.AddNewPerson_Button.Click += new System.EventHandler(this.AddNewPerson_Button_Click);
+            // 
+            // DeletePerson_Button
+            // 
+            this.DeletePerson_Button.Location = new System.Drawing.Point(510, 279);
+            this.DeletePerson_Button.Name = "DeletePerson_Button";
+            this.DeletePerson_Button.Size = new System.Drawing.Size(75, 23);
+            this.DeletePerson_Button.TabIndex = 14;
+            this.DeletePerson_Button.Text = "Delete";
+            this.DeletePerson_Button.UseVisualStyleBackColor = true;
+            this.DeletePerson_Button.Click += new System.EventHandler(this.DeletePerson_Button_Click);
+            // 
+            // AddNewContact_Button
+            // 
+            this.AddNewContact_Button.Location = new System.Drawing.Point(510, 197);
+            this.AddNewContact_Button.Name = "AddNewContact_Button";
+            this.AddNewContact_Button.Size = new System.Drawing.Size(75, 23);
+            this.AddNewContact_Button.TabIndex = 15;
+            this.AddNewContact_Button.Text = "Add";
+            this.AddNewContact_Button.UseVisualStyleBackColor = true;
+            this.AddNewContact_Button.Click += new System.EventHandler(this.AddNewContact_Button_Click);
+            // 
+            // AddNewContact_TextBox
+            // 
+            this.AddNewContact_TextBox.Location = new System.Drawing.Point(162, 197);
+            this.AddNewContact_TextBox.Name = "AddNewContact_TextBox";
+            this.AddNewContact_TextBox.Size = new System.Drawing.Size(320, 21);
+            this.AddNewContact_TextBox.TabIndex = 16;
+            // 
+            // PhoneType_ComboBox
+            // 
+            this.PhoneType_ComboBox.FormattingEnabled = true;
+            this.PhoneType_ComboBox.Items.AddRange(new object[] {
+            "Mobile",
+            "Work",
+            "Home"});
+            this.PhoneType_ComboBox.Location = new System.Drawing.Point(18, 197);
+            this.PhoneType_ComboBox.Name = "PhoneType_ComboBox";
+            this.PhoneType_ComboBox.Size = new System.Drawing.Size(121, 22);
+            this.PhoneType_ComboBox.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 565);
+            this.Controls.Add(this.PhoneType_ComboBox);
+            this.Controls.Add(this.AddNewContact_TextBox);
+            this.Controls.Add(this.AddNewContact_Button);
+            this.Controls.Add(this.DeletePerson_Button);
+            this.Controls.Add(this.AddNewPerson_Button);
+            this.Controls.Add(this.AddNewPersonName_TextBox);
+            this.Controls.Add(this.AddNewPersonAddress_TextBox);
+            this.Controls.Add(this.AddNewContactID_TextBox);
+            this.Controls.Add(this.DeleteContactID_TextBox);
+            this.Controls.Add(this.AddNewPersonID_TextBox);
+            this.Controls.Add(this.DeleteContactID_Label);
+            this.Controls.Add(this.AddNewContactID_Label);
+            this.Controls.Add(this.AddPersonID_Label);
+            this.Controls.Add(this.AddNewPersonName_Label);
+            this.Controls.Add(this.AddNewPersonAddress_Label);
+            this.Controls.Add(this.AddNewContact_Label);
+            this.Controls.Add(this.DeleteContact_Label);
+            this.Controls.Add(this.AddNewPerson_Label);
+            this.Font = new System.Drawing.Font("Quartz MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label AddNewPerson_Label;
+        private System.Windows.Forms.Label DeleteContact_Label;
+        private System.Windows.Forms.Label AddNewContact_Label;
+        private System.Windows.Forms.Label AddNewPersonAddress_Label;
+        private System.Windows.Forms.Label AddNewPersonName_Label;
+        private System.Windows.Forms.Label AddPersonID_Label;
+        private System.Windows.Forms.Label AddNewContactID_Label;
+        private System.Windows.Forms.Label DeleteContactID_Label;
+        private System.Windows.Forms.TextBox AddNewPersonID_TextBox;
+        private System.Windows.Forms.TextBox DeleteContactID_TextBox;
+        private System.Windows.Forms.TextBox AddNewContactID_TextBox;
+        private System.Windows.Forms.TextBox AddNewPersonAddress_TextBox;
+        private System.Windows.Forms.TextBox AddNewPersonName_TextBox;
+        private System.Windows.Forms.Button AddNewPerson_Button;
+        private System.Windows.Forms.Button DeletePerson_Button;
+        private System.Windows.Forms.Button AddNewContact_Button;
+        private System.Windows.Forms.TextBox AddNewContact_TextBox;
+        private System.Windows.Forms.ComboBox PhoneType_ComboBox;
     }
 }
 
